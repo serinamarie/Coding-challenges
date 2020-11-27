594027bbca07ae688de43ea8b8a64af5682083d6
Serina Grill <gril0044@umn.edu>
2020-11-27 15:44:42 -0500

    added consecutive chas

---
+++ strings/consecutive_characters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        # output: len of max substring of consecutive char as int
+        # input: str
+        # constraints: length from 1-500, only lowercase english letters
+        # edge cases: must work for length of 1
+        
+        # keep track of the max length as we go, and also have a current max length
+        # greatest max = 1
+        # loop through string (while next char)
+        # if next char is equal to current char:
+            # increase local max
+            # if local max is greater than the greatest max
+                # greatest max = greatest max
+        # return greatest max
+        
+        greatest_max = 1
+        local_max = 1
+        
+        for i in range(len(s)-1):
+            if s[i+1] == s[i]:
+                local_max += 1
+                if local_max > greatest_max:
+                    greatest_max = local_max
+            else:
+                local_max = 1
+                
+        return greatest_max
+                
