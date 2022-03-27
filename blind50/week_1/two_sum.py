8659afc0b7f00a7d042b300d8ab30325672b4b55
Serina Grill <Serina.Grill@aa.com>
2022-03-27 14:38:15 -0700

    Adding leetcode link

--- blind50/week_1/two_sum.py
+++ blind50/week_1/two_sum.py
@@ -3,7 +3,7 @@
 # have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
 
-
+# https://leetcode.com/problems/two-sum/
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
