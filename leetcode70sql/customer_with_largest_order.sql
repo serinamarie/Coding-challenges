48c2755b28062d1117c091a86f1ffd3f960f36cf
Serina Grill <Serina.Grill@aa.com>
2022-03-31 16:45:35 -0700

    adding customer with largest order

---
+++ leetcode70sql/customer_with_largest_order.sql
@@ -0,0 +1,53 @@
+Table: Orders
+
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+ 
+
+Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+
+The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+The query result format is in the following example.
+
+
+Example 1:
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+Explanation: 
+The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+So the result is customer_number 3.
+ 
+
+# Write your MySQL query statement below
+
+
+WITH temp_table 
+AS (SELECT *, COUNT(order_number) OVER (PARTITION BY customer_number) AS nbr_orders
+FROM Orders)
+
+SELECT customer_number
+FROM temp_table
+ORDER BY nbr_orders DESC
+LIMIT 1
